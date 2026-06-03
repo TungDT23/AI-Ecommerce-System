98210ab356e97f9810f3a87845f468ac5d537bdc
Tung Thanh <tungdangthanh23032004@gmail.com>
2026-06-04 00:45:02 +0700

    Delete test file

--- ai-service/test_ai.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-payload = {
-    "userId": 9003,
-    "lastPurchasedCategoryId": 4
-}
-res = requests.post("http://localhost:8000/ai/predict", json=payload)
-print(res.status_code, res.text)
